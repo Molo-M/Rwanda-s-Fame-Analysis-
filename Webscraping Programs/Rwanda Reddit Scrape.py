b22b274e0618224f1d9eb6092be87c6eba5ab0a3
Molo Munyansanga <molomunyansanga@gmail.com>
2021-12-19 08:28:14 +0200

    Python code

--- Webscraping Programs/Rwanda Reddit Scrape.py
+++ Webscraping Programs/Rwanda Reddit Scrape.py
@@ -1,9 +1,11 @@
+# Import useful libraries
 import praw as pr
 import pandas as pd
 from datetime import datetime
 
+# authorization for reddit api
 user_agent = 'Scraper 1.0 by /u/python_for_fun'
-reddit = pr.Reddit(client_id='80wTfrz5Tr1JooibGrOU6g', client_secret='L09Cc94-y1MigcafPnn_9XSFxo42Vw',
+reddit = pr.Reddit(client_id='**************', client_secret='**************',
                    user_agent=user_agent)
 
 name = list()
@@ -11,6 +13,7 @@ link = list()
 year = list()
 number = 0
 
+# Begin scraping the information
 for submission in reddit.subreddit('Rwanda').new(limit=None):
     # print(submission.title)
     name.append(submission.title)
@@ -29,4 +32,5 @@ reddit_dict = {'Title': name, 'Year': year, 'URL': link}
 df = pd.DataFrame(reddit_dict)
 print(df)
 
-# df.to_csv(r'C:\Users\HP\PycharmProjects\pythonProject' + '\\Rwanda_Reddit_Data.csv', index=False)
+# save file to local pc
+df.to_csv(r'C:\Users\HP\PycharmProjects\pythonProject' + '\\Rwanda_Reddit_Data.csv', index=False)
